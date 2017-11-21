2d25793c964746c230973b0fd67b8a0f35720dfa
Johan Lindh <johan@linkdata.se>
2017-11-21 09:53:21 +0100

    Revert refactor

--- go/framedata.go
+++ go/framedata.go
@@ -4,6 +4,12 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"path"
+	"strconv"
+	"strings"
 )
 
 var (
@@ -44,4 +50,239 @@ func (fd FrameData) Available() int {
 // Buffered returns the number of bytes that have been written to the frame.
 func (fd FrameData) Buffered() int {
 	return len(fd)
+}
+
+// WriteHeader initializes the frame header.
+func (fd *FrameData) WriteHeader(exchangeID ExchangeID) {
+	*fd = AppendFrameHeader((*fd)[:0], exchangeID)
+	return
+}
+
+// WriteUint64 writes an uint64 to a FrameData using a portable encoding.
+func (fd *FrameData) WriteUint64(x uint64) {
+	for x >= 0x80 {
+		*fd = append(*fd, byte(x)|0x80)
+		x >>= 7
+	}
+	*fd = append(*fd, byte(x))
+	return
+}
+
+// WriteInt64 writes an int64 to a FrameData using a portable encoding.
+func (fd *FrameData) WriteInt64(x int64) {
+	if x >= 0 {
+		fd.WriteUint64(uint64(x) << 1)
+	} else {
+		fd.WriteUint64(uint64(-x)<<1 | 1)
+	}
+	return
+}
+
+// WriteLen writes a nonnegative integer less than 0x8000 to a FrameData
+// using a portable encoding.
+func (fd *FrameData) WriteLen(x int) error {
+	switch {
+	case x < 0:
+		return ErrLengthNegative
+	case x < 0x80:
+		*fd = append(*fd, byte(x))
+	case x < 0x7fff:
+		*fd = append(*fd, byte(x>>8)|0x80, byte(x))
+	default:
+		return ErrLengthOverflow
+	}
+	return nil
+}
+
+// WriteString writes a string to a FrameData. The string must be
+// less than 0x8000 bytes long.
+func (fd *FrameData) WriteString(s string) {
+	if len(s) == 0 {
+		*fd = append(*fd, byte(0), byte(1))
+		return
+	}
+	// TODO implement string lookup
+	if fd.WriteLen(len(s)) == nil {
+		*fd = append(*fd, s...)
+	}
+	return
+}
+
+// WriteByteArrayString writes a byte array that is considered a string
+// to a FrameData. The byte array must be less than 0x8000 bytes long.
+func (fd *FrameData) WriteByteArrayString(ba []byte) {
+	if len(ba) == 0 {
+		*fd = append(*fd, byte(0), byte(1))
+		return
+	}
+	// TODO implement string lookup
+	if fd.WriteLen(len(ba)) == nil {
+		*fd = append(*fd, ba...)
+	}
+	return
+}
+
+// WriteStringNull writes a NULL string to a FrameData. A NULL string
+// is used as a marker is the protocol and is distinct from an empty
+// string.
+func (fd *FrameData) WriteStringNull() {
+	*fd = append(*fd, byte(0), byte(0))
+	return
+}
+
+// WriteByte appends a single byte.
+func (fd *FrameData) WriteByte(b byte) error {
+	*fd = append(*fd, b)
+	return nil
+}
+
+// WriteRecordType writes a frame record type constant.
+func (fd *FrameData) WriteRecordType(rt RecordType) {
+	*fd = append(*fd, byte(rt))
+}
+
+// ByteCount returns the number of bytes in the FrameData as an uint64.
+func (fd FrameData) ByteCount() uint64 {
+	return uint64(len(fd))
+}
+
+// ReadFrom reads a FrameData from an io.Reader.
+// Implements io.ReaderFrom interface for FrameData.
+func (fd *FrameData) ReadFrom(r io.Reader) (n int64, err error) {
+	var num int // needed to let ReadFrom/ReadFull integrate well.
+
+	num, err = io.ReadFull(r, (*fd)[:FrameHeaderSize])
+	n = int64(num)
+	if err == nil && fd.Header().HasPayload() {
+		// if below line panics it means we got a frame larger than FrameMaxSize
+		*fd = (*fd)[:FrameHeaderSize+int(fd.Header().SizeValue())]
+		num, err = io.ReadFull(r, (*fd)[FrameHeaderSize:])
+		n += int64(num)
+	}
+
+	return
+}
+
+// ErrFrameTooBig means a frame with more than FrameMaxPayloadSize bytes occured
+var ErrFrameTooBig = errors.New("rap: frame too big")
+
+// WriteTo implements io.WriterTo for FrameData.
+func (fd FrameData) WriteTo(w io.Writer) (int64, error) {
+	if fd.Header().HasPayload() {
+		payloadLength := int32(len(fd)) - FrameHeaderSize
+		if payloadLength < 0 {
+			log.Fatal("FrameData.WriteTo(): negative payload length")
+		}
+		if payloadLength > FrameMaxPayloadSize {
+			return 0, ErrFrameTooBig
+		}
+		fd.Header().SetSizeValue(payloadLength)
+	}
+	// log.Print("FrameData.WriteTo(): ", fd)
+	n := 0
+	for n < len(fd) {
+		m, err := w.Write(fd[n:])
+		n += m
+		if err != nil {
+			return int64(n), err
+		}
+	}
+	return int64(n), nil
+}
+
+// WriteRequest writes a FrameTypeRequest record to a FrameData given a http.Request.
+func (fd *FrameData) WriteRequest(r *http.Request) error {
+	fd.Header().SetHead()
+	fd.WriteRecordType(RecordTypeHTTPRequest)
+	fd.WriteString(r.Method)
+
+	np := path.Clean(strings.Replace(r.URL.Path, "\\", "/", -1))
+	if np != "/" && r.URL.Path[len(r.URL.Path)-1] == '/' {
+		np += "/"
+	}
+	fd.WriteString(np)
+
+	qv := r.URL.Query()
+	for k, vv := range qv {
+		fd.WriteString(k)
+		for _, v := range vv {
+			fd.WriteString(v)
+		}
+		fd.WriteStringNull()
+	}
+	fd.WriteStringNull()
+
+	contentLength := r.ContentLength
+	haveContentLengthHeader := false
+	host := r.Host
+	haveHostHeader := false
+	for k, vv := range r.Header {
+		if !haveContentLengthHeader && k == "Content-Length" {
+			haveContentLengthHeader = true
+			if contentLength < 0 {
+				if n, err := strconv.ParseInt(vv[0], 10, 64); err == nil {
+					contentLength = n
+				}
+			}
+			continue
+		}
+		if !haveHostHeader && k == "Host" {
+			haveHostHeader = true
+			if host == "" {
+				host = vv[0]
+			}
+			continue
+		}
+		fd.WriteString(k)
+		for _, v := range vv {
+			fd.WriteString(v)
+		}
+		fd.WriteStringNull()
+	}
+	fd.WriteStringNull()
+	if host == "" {
+		fd.WriteStringNull()
+	} else {
+		fd.WriteString(host)
+	}
+	if haveContentLengthHeader || contentLength > 0 {
+		fd.WriteInt64(contentLength)
+	} else {
+		fd.WriteInt64(-1)
+	}
+	if len(*fd) > FrameMaxSize {
+		return ErrFrameTooBig
+	}
+	return nil
+}
+
+// WriteResponse writes a FrameTypeResponse record to a FrameData given a http.Header.
+func (fd *FrameData) WriteResponse(code int, contentLength int64, header http.Header) error {
+	// log.Print("FrameData.WriteResponse(", code, ", ", contentLength, ", ", header)
+	fd.Header().SetHead()
+	fd.WriteRecordType(RecordTypeHTTPResponse)
+	fd.WriteLen(code)
+	statusText := ""
+	for k, vv := range header {
+		if k == "Status" {
+			statusText = vv[0]
+			continue
+		}
+		fd.WriteString(k)
+		for _, v := range vv {
+			fd.WriteString(v)
+		}
+		fd.WriteStringNull()
+	}
+	fd.WriteStringNull()
+	if statusText == "" {
+		fd.WriteStringNull()
+	} else {
+		fd.WriteString(statusText)
+	}
+	fd.WriteInt64(contentLength)
+	if len(*fd) > FrameMaxSize {
+		return io.ErrShortBuffer
+	}
+	return nil
 }
