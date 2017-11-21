45158eda645ce2f2522920132ab43d25be25c1c3
Johan Lindh <johan@linkdata.se>
2017-11-21 09:55:43 +0100

    Fold in the *_http files now that fasthttp is gone

--- go/exchange.go
+++ go/exchange.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net/http"
+	"runtime/debug"
 	"time"
 )
 
@@ -418,4 +420,114 @@ func (e *Exchange) Release() {
 	// log.Print("Exchange.Release() ", e)
 	e.Stop()
 	e.Conn.releaseExchange(e)
+}
+
+// WriteRequest writes a http.Request to the exchange, including it's Body and
+// a final frame.
+func (e *Exchange) WriteRequest(r *http.Request) error {
+	// log.Print("Exchange.WriteRequest() ", e)
+	e.writeStart()
+
+	if err := e.fdw.WriteRequest(r); err != nil {
+		log.Print("Exchange.WriteRequest(): fdw.WriteRequest: ", err.Error(), " e.fdw=", e.fdw)
+		return err
+	}
+
+	if _, err := e.ReadFrom(r.Body); err != nil {
+		log.Print("Exchange.WriteRequest(): ReadFrom(r.Body): ", err.Error())
+		return err
+	}
+
+	if err := e.CloseWrite(); err != nil {
+		log.Print("Exchange.WriteRequest(): CloseWrite: ", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+// ProxyResponse reads a HTTP response and it's body from the Exchange data
+// and writes it to the given http.ResponseWriter.
+func (e *Exchange) ProxyResponse(w http.ResponseWriter) error {
+	// log.Print("Exchange.ProxyResponse() ", e)
+	if err := e.loadFrameReader(); err != nil {
+		return err
+	}
+
+	if !e.fdr.Header().HasHead() {
+		return ErrMissingFrameHead
+	}
+
+	if e.fr.ReadRecordType() != RecordTypeHTTPResponse {
+		return ErrUnhandledRecordType
+	}
+
+	e.fr.ProxyResponse(w)
+
+	if _, err := e.WriteTo(w); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// WriteResponse writes a http.Response to the exchange.
+func (e *Exchange) WriteResponse(r *http.Response) (err error) {
+	// log.Print("Exchange.WriteResponse() ", e)
+	return e.WriteResponseData(r.StatusCode, r.ContentLength, r.Header)
+}
+
+// WriteResponseData writes a RAP response header.
+func (e *Exchange) WriteResponseData(code int, contentLength int64, header http.Header) (err error) {
+	// log.Print("Exchange.WriteResponseData() ", e)
+	e.writeStart()
+	return e.fdw.WriteResponse(code, contentLength, header)
+}
+
+func serveHTTP(h http.Handler, rw *ResponseWriter, req *http.Request) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Print("Exchange.serveHTTP(): ", r)
+			debug.PrintStack()
+		}
+	}()
+	h.ServeHTTP(rw, req)
+}
+
+func (e *Exchange) serve(h http.Handler) {
+	defer e.Release()
+	for {
+		if err := e.Start(h); err != nil {
+			if err != io.EOF {
+				log.Print("Exchange.serve() ", e.ID, ": ", err)
+			}
+			return
+		}
+		e.Stop()
+	}
+}
+
+// Start waits for a start frame and then invokes the given http.Handler.
+func (e *Exchange) Start(h http.Handler) error {
+	// log.Print("Exchange.Start() ", e)
+	if err := e.loadFrameReader(); err != nil {
+		return err
+	}
+	if !e.fdr.Header().HasHead() {
+		return ErrMissingFrameHead
+	}
+	e.hasStarted = true
+	e.hasReceived = true
+	switch e.fr.ReadRecordType() {
+	case RecordTypeHTTPRequest:
+		req, err := e.fr.ReadRequest()
+		if err != nil {
+			return err
+		}
+		req.Body = e
+		h.ServeHTTP(&ResponseWriter{Exchange: e}, req)
+	default:
+		return ErrUnhandledRecordType
+	}
+	return nil
 }
--- go/gateway.go
+++ go/gateway.go
@@ -1,8 +1,115 @@
 package rap
 
+import (
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+)
+
 // Gateway maintains one or more Streams to the upstream server.
 // A Gateway receives incoming HTTP requests and multiplexes these
 // onto one or more Streams. It may create new Streams as needed.
 type Gateway struct {
 	Client *Client
+}
+
+func proxyFrameData(w http.ResponseWriter, fd FrameData) {
+	/*
+		fr := NewFrameReader(fd)
+		if fd.Header().HasHead() {
+			fr.ProxyResponse(w)
+		}
+		if fd.Header().HasBody() {
+			if err := fr.ProxyBody(w); err != nil {
+				log.Print("Exchange.proxyResponse() ProxyBody() ", err.Error())
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+	*/
+}
+
+func (g *Gateway) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// log.Print("Gateway.ServeHTTP() ", r)
+	e := g.Client.NewExchange()
+
+	if e == nil {
+		var err error
+		e, err = g.Client.NewExchangeMayDial()
+		if e == nil {
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusGatewayTimeout)
+			} else {
+				log.Print("Gateway.ServeHTTP(): can't allocate exchange and nil error")
+				http.Error(w, "can't allocate exchange", http.StatusInternalServerError)
+			}
+			return
+		}
+	}
+
+	defer e.Release() // will do Stop() before release
+
+	// Detect and handle WebSocket requests.
+	if len(r.Header["Upgrade"]) > 0 &&
+		len(r.Header["Connection"]) > 0 &&
+		r.ProtoAtLeast(1, 1) &&
+		r.Method == "GET" &&
+		strings.ToLower(r.Header["Upgrade"][0]) == "websocket" &&
+		strings.ToLower(r.Header["Connection"][0]) == "upgrade" {
+		hj, ok := w.(http.Hijacker)
+		if !ok {
+			http.Error(w, "Gateway.ServeHTTP(): http.Hijacker unsupported", http.StatusInternalServerError)
+			return
+		}
+		rwc, buf, err := hj.Hijack()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer rwc.Close()
+		br := buf.Reader
+		if br.Buffered() > 0 {
+			log.Print("Gateway.ServeHTTP(): websocket client sent data before handshake was complete")
+			return
+		}
+		e.initiateWebsocket(rwc, buf, r)
+		return
+	}
+
+	var requestErr error
+	var responseErr error
+
+	if r.ContentLength == 0 {
+		// we can run this without a separate goroutine as request has no body.
+		requestErr = e.WriteRequest(r)
+		responseErr = e.ProxyResponse(w)
+	} else {
+		// we allow a response to send before request has finished sending,
+		// useful when the upstream does stream processing (such as echoing).
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			requestErr = e.WriteRequest(r)
+		}()
+		responseErr = e.ProxyResponse(w)
+		wg.Wait()
+	}
+
+	if responseErr == nil && requestErr == nil {
+		return
+	}
+
+	errorText := "Gateway.ServeHTTP():"
+	if requestErr != nil {
+		errorText += " requestErr=" + requestErr.Error()
+	}
+	if responseErr != nil {
+		errorText += " responseErr=" + responseErr.Error()
+	}
+	log.Print(errorText)
+	if !e.hasReceived {
+		http.Error(w, errorText, http.StatusInternalServerError)
+	}
 }
