2642ad2789822686e83686594e02140a2bc4982c
Johan Lindh <johan@linkdata.se>
2017-11-28 15:21:25 +0100

    More tests

--- go/exchange_test.go
+++ go/exchange_test.go
@@ -1,6 +1,7 @@
 package rap
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -68,9 +69,91 @@ func Test_Exchange_String(t *testing.T) {
 }
 
 func Test_Exchange_StartAndRelease(t *testing.T) {
+	// Simple case
 	et := newExchangeTester(t)
 	et.InjectRequest(httptest.NewRequest("GET", "/", nil))
 	et.Exchange.Start(et)
 	et.Exchange.Release()
 	assert.True(t, et.released)
+
+	// EOF before starting
+	et = newExchangeTester(t)
+	close(et.readCh)
+	err := et.Exchange.Start(et)
+	assert.Equal(t, io.EOF, err)
+	et.Exchange.Release()
+	assert.True(t, et.released)
+
+	// Empty frame
+	et = newExchangeTester(t)
+	fd := NewFrameData()
+	fd.Header().SetFinal()
+	et.readCh <- fd
+	err = et.Exchange.Start(et)
+	assert.Equal(t, io.EOF, err)
+	et.Exchange.Release()
+	assert.True(t, et.released)
+
+	// Empty frame sequence
+	et = newExchangeTester(t)
+	fd = NewFrameData()
+	et.readCh <- fd
+	fd = NewFrameData()
+	fd.Header().SetFinal()
+	et.readCh <- fd
+	err = et.Exchange.Start(et)
+	assert.Equal(t, io.EOF, err)
+	et.Exchange.Release()
+	assert.True(t, et.released)
+
+	// Missing frame head
+	et = newExchangeTester(t)
+	fd = NewFrameData()
+	fd.WriteRecordType(RecordTypeHTTPRequest)
+	fd.Header().SetFinal()
+	et.readCh <- fd
+	err = et.Exchange.Start(et)
+	assert.Equal(t, ErrMissingFrameHead, err)
+	et.Exchange.Release()
+	assert.True(t, et.released)
+
+	// Invalid URL in request record
+	et = newExchangeTester(t)
+	fd = NewFrameData()
+	fd.Header().SetHead()
+	fd.WriteRecordType(RecordTypeHTTPRequest)
+	fd.WriteStringNull()  // method
+	fd.WriteString(":a:") // illegal url
+	fd.Header().SetFinal()
+	et.readCh <- fd
+	err = et.Exchange.Start(et)
+	assert.Error(t, err)
+	et.Exchange.Release()
+	assert.True(t, et.released)
+
+	// Incomplete request frame
+	et = newExchangeTester(t)
+	fd = NewFrameData()
+	fd.Header().SetHead()
+	fd.WriteRecordType(RecordTypeHTTPRequest)
+	fd.Header().SetFinal()
+	et.readCh <- fd
+	assert.Panics(t, func() {
+		et.Exchange.Start(et)
+	})
+	et.Exchange.Release()
+	assert.True(t, et.released)
+
+	// Illegal record type
+	et = newExchangeTester(t)
+	fd = NewFrameData()
+	fd.Header().SetHead()
+	fd.WriteRecordType(RecordTypeUserFirst - 1)
+	fd.Header().SetFinal()
+	et.readCh <- fd
+	err = et.Exchange.Start(et)
+	assert.Equal(t, ErrUnhandledRecordType, err)
+	et.Exchange.Release()
+	assert.True(t, et.released)
+
 }
