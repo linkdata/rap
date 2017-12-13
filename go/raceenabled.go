961eb0074eae186cf15f9800ab98e6e3f6ca37ee
Johan Lindh <johan@linkdata.se>
2017-12-13 18:22:23 +0100

    Limit MaxExchangeID so as to not overload race detector

--- go/raceenabled.go
+++ go/raceenabled.go
@@ -8,4 +8,7 @@ package rap
 
 func init() {
 	raceEnabled = true
+	// race detector can only handle max of 8192 goroutines,
+	// and we'll need to run two Conn's at the same time.
+	MaxExchangeID = ExchangeID(3072)
 }
