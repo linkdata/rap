cf823193d8dac77cec801360acae7e8d368aa878
Johan Lindh <johan@linkdata.se>
2017-12-14 10:54:04 +0100

    Test basic request/response

--- go/conn_test.go
+++ go/conn_test.go
@@ -3,8 +3,12 @@ package rap
 import (
 	"io"
 	"net/http"
+	"net/http/httptest"
+	"sync"
 	"testing"
+	"time"
 
+	"github.com/fortytw2/leaktest"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,6 +16,8 @@ type rwcPipe struct {
 	*io.PipeReader
 	*io.PipeWriter
 }
+
+const connTimeout = time.Millisecond * time.Duration(10)
 
 func (rwcp *rwcPipe) Close() error {
 	if err := rwcp.PipeWriter.Close(); err != nil {
@@ -35,37 +41,72 @@ func newRwcPipes() (a, b *rwcPipe) {
 }
 
 type connTester struct {
+	t        *testing.T
 	a, b     *rwcPipe
 	conn     *Conn
 	server   *Conn
 	isClosed bool
+	done     chan struct{}
 }
 
 func (ct *connTester) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(200)
 }
 
-func (ct *connTester) Close() (err error) {
-	return ct.a.Close()
+func (ct *connTester) Close() {
+	if !ct.isClosed {
+		ct.isClosed = true
+		assert.NoError(ct.t, ct.a.Close())
+		<-ct.done
+	}
 }
 
-func newConnTester() (ct *connTester) {
+func newConnTester(t *testing.T) (ct *connTester) {
 	a, b := newRwcPipes()
 	ct = &connTester{
+		t:      t,
 		a:      a,
 		b:      b,
 		conn:   NewConn(a),
 		server: NewConn(b),
+		done:   make(chan struct{}),
 	}
 	go func(ct *connTester) {
-		if err := ct.server.ServeHTTP(ct); err != nil {
-			return
-		}
+		assert.NoError(ct.t, ct.server.ServeHTTP(ct))
+		close(ct.done)
 	}(ct)
+	go ct.conn.ServeHTTP(nil)
 	return
 }
 
+func (ct *connTester) InjectRequest(r *http.Request) {
+	w := httptest.NewRecorder()
+	var requestErr error
+	var responseErr error
+	var wg sync.WaitGroup
+	e := ct.conn.NewExchangeWait(connTimeout)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		requestErr = e.WriteRequest(r)
+	}()
+	responseErr = e.ProxyResponse(w)
+	wg.Wait()
+	assert.NoError(ct.t, requestErr)
+	assert.NoError(ct.t, responseErr)
+}
+
 func Test_Conn_String(t *testing.T) {
-	ct := newConnTester()
+	defer leaktest.Check(t)()
+	ct := newConnTester(t)
 	defer ct.Close()
 	assert.Equal(t, "[Conn]", ct.conn.String())
+}
+
+func Test_Conn_normal_request_response(t *testing.T) {
+	// defer leaktest.Check(t)()
+	MaxExchangeID = ExchangeID(1)
+	ct := newConnTester(t)
+	defer ct.Close()
+	ct.InjectRequest(httptest.NewRequest("GET", "/", nil))
 }
