610269491f5427a3e3e2eaa558561c104d112a58
Johan Lindh <johan@linkdata.se>
2017-12-18 15:40:30 +0100

    Test_Server_serve_errors

--- server_test.go
+++ server_test.go
@@ -1,7 +1,11 @@
 package rap
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_Server_support_functions(t *testing.T) {
 	s := &Server{}
@@ -14,4 +18,24 @@ func Test_Server_support_functions(t *testing.T) {
 	s.AddBytesWritten(2)
 	assert.Equal(t, int64(1), s.BytesRead())
 	assert.Equal(t, int64(2), s.BytesWritten())
+}
+
+func Test_Server_serve_errors(t *testing.T) {
+	gt := &gwTester{}
+	srv := &Server{
+		Addr:    srvAddr,
+		Handler: gt,
+	}
+	go srv.ListenAndServe()
+	gw := NewGateway(srvAddr)
+	rr := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	gw.ServeHTTP(rr, r)
+	gw.Client.getConn().CloseWrite()
+	rr = httptest.NewRecorder()
+	r = httptest.NewRequest("GET", "/", nil)
+	gw.ServeHTTP(rr, r)
+	srv.listener.Close()
+	em := srv.ServeErrors()
+	assert.Equal(t, 1, len(em))
 }
