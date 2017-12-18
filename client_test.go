a1e3fa532f05c7f2f6bd745c122d799d3f59f86a
Johan Lindh <johan@linkdata.se>
2017-12-18 13:23:02 +0100

    make addr a const

--- client_test.go
+++ client_test.go
@@ -4,13 +4,15 @@ import "testing"
 import "github.com/stretchr/testify/assert"
 import "time"
 
+const noSrvAddr string = "192.0.2.1:1"
+
 func Test_Client_NewClient(t *testing.T) {
-	c := NewClient("")
+	c := NewClient(noSrvAddr)
 	assert.NotNil(t, c)
 }
 
 func Test_Client_no_answer(t *testing.T) {
-	c := NewClient("192.0.2.1:1")
+	c := NewClient(noSrvAddr)
 	c.DialTimeout = time.Millisecond * 10
 	e, err := c.NewExchangeMayDial()
 	assert.Nil(t, e)
@@ -18,7 +20,7 @@ func Test_Client_no_answer(t *testing.T) {
 }
 
 func Test_Client_server_seems_offline(t *testing.T) {
-	c := NewClient("192.0.2.1:1")
+	c := NewClient(noSrvAddr)
 	c.DialTimeout = time.Millisecond * 10
 	c.firstAttempt = time.Now().Add(-time.Second)
 	e, err := c.NewExchangeMayDial()
