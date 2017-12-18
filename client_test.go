b8c9620a32527a681ee39ceeacdcb98a695ed36a
Johan Lindh <johan@linkdata.se>
2017-12-18 12:59:04 +0100

    Add some error testing

--- client_test.go
+++ client_test.go
@@ -2,8 +2,26 @@ package rap
 
 import "testing"
 import "github.com/stretchr/testify/assert"
+import "time"
 
 func Test_Client_NewClient(t *testing.T) {
 	c := NewClient("")
 	assert.NotNil(t, c)
+}
+
+func Test_Client_no_answer(t *testing.T) {
+	c := NewClient("192.0.2.1:1")
+	c.DialTimeout = time.Millisecond * 10
+	e, err := c.NewExchangeMayDial()
+	assert.Nil(t, e)
+	assert.Error(t, err)
+}
+
+func Test_Client_server_seems_offline(t *testing.T) {
+	c := NewClient("192.0.2.1:1")
+	c.DialTimeout = time.Millisecond * 10
+	c.firstAttempt = time.Now().Add(-time.Second)
+	e, err := c.NewExchangeMayDial()
+	assert.Nil(t, e)
+	assert.Error(t, err)
 }
