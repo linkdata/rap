5d4358598e556c12a8025ee19f18bbd120e37409
Johan Lindh <johan@linkdata.se>
2018-01-11 13:38:08 +0100

    Add Test_ReverseProxy_ServeHTTP

--- reverseproxy_test.go
+++ reverseproxy_test.go
@@ -1,6 +1,9 @@
 package rap
 
 import (
+	"bytes"
+	"io/ioutil"
+	"net/http/httptest"
 	"net/url"
 	"testing"
 
@@ -11,4 +14,19 @@ func Test_ReverseProxy_NewReverseProxy(t *testing.T) {
 	rp := NewReverseProxy(&url.URL{}, 0)
 	assert.NotNil(t, rp)
 	assert.NotZero(t, cap(rp.transports))
+}
+
+func Test_ReverseProxy_ServeHTTP(t *testing.T) {
+	et := newExchangeTester(t)
+	defer et.Close()
+	hs := httptest.NewServer(et)
+	defer hs.Close()
+	u, e := url.Parse(hs.URL)
+	assert.NoError(t, e)
+	rp := NewReverseProxy(u, 0)
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest("PUT", "/foo/?bar=quux&bar=foo", ioutil.NopCloser(bytes.NewBufferString("Hello world body!")))
+	req.Header.Add("Connection", "keep-alive")
+	req.ContentLength = -1
+	rp.ServeHTTP(rr, req)
 }
